0df4458e7c25e5d0f1cfbc7031bbcac46b252c65
Kang Hyun Woong <cdcd8894@naver.com>
2018-01-04 09:50:47 +0900

    merge
    merge

--- Final_Mission/prototype/Assets/test/movetest.cs
+++ Final_Mission/prototype/Assets/test/movetest.cs
@@ -7,7 +7,6 @@ public class movetest : MonoBehaviour {
 	int i = 0 ;
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
@@ -16,8 +15,10 @@ public class movetest : MonoBehaviour {
 		this.transform.Translate (Vector3.forward*3*Time.deltaTime);
 
 		if (Vector3.Distance (this.transform.position, next [i].transform.position) < 0.05f) {
-			if(i<5)
+			if (i < 8)
 				++i;
+			else
+				i = 1;
 		}
 	}
 }
