b79a599f98c7f3acb589ec78f5331739cdb95070
wooloves39 <wooloves39@users.noreply.github.com>
2018-01-05 18:13:33 +0900

    대화 상자 끝!!!!!
    스토리 받아서 바로 적용하면 됨

--- Final_Mission/prototype/Assets/Hyun/Use/Dialog.cs
+++ Final_Mission/prototype/Assets/Hyun/Use/Dialog.cs
@@ -22,25 +22,35 @@ public class Dialog : MonoBehaviour
     public GameObject ContinueIcon;
     public GameObject StopIcon;
 
+    public GameObject Me;
+    public GameObject Boss;
     void Start()
     {
+        DialogueStrings = new string[5];
+        DialogueStrings[0] = "야 임마";
+        DialogueStrings[1] = "잘해라!";
+        DialogueStrings[2] = "바빠도 열심히 하고!";
+        DialogueStrings[3] = "안 바쁘면 더 열심히하고!";
+        DialogueStrings[4] = "졸업하고 취업하자!";
         _textComponent = GetComponent<Text>();
         _textComponent.text = "";
-
+        moveSetting();
         HideIcons();
+        _isDialoguePlaying = true;
+        StartCoroutine(StartDialogue());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
-            if (!_isStringBeingRevealed)
-            {
-                _isDialoguePlaying = true;
-                StartCoroutine(StartDialogue());
-            }
-        }
+        //if (Input.GetKeyDown(KeyCode.Return))
+        //{
+        //    if (!_isStringBeingRevealed)
+        //    {
+        //        _isDialoguePlaying = true;
+        //        StartCoroutine(StartDialogue());
+        //    }
+        //}
     }
     private IEnumerator StartDialogue()
     {
@@ -51,6 +61,7 @@ public class Dialog : MonoBehaviour
             if (!_isStringBeingRevealed)
             {
                 _isStringBeingRevealed = true;
+                moveImage(currentDialogueIndex);
                 StartCoroutine(DisplatStrings(DialogueStrings[currentDialogueIndex++]));
 
                 if (currentDialogueIndex >= dialogueLengh)
@@ -82,7 +93,7 @@ public class Dialog : MonoBehaviour
                 if (Input.GetKey(DialogueInput))
                 {
 
-                    yield return new WaitForSeconds(SecondsBetweenCharacters*CharacterRateMultuplier);
+                    yield return new WaitForSeconds(SecondsBetweenCharacters * CharacterRateMultuplier);
                 }
                 else
                 {
@@ -115,5 +126,21 @@ public class Dialog : MonoBehaviour
             return;
         }
         ContinueIcon.SetActive(true);
+    }
+    private void moveSetting()
+    {
+        iTween.MoveFrom(Me, iTween.Hash("x", Me.transform.position.x - 10.0f, "time", 1.0f, "easetype", iTween.EaseType.easeOutBounce));
+        iTween.MoveFrom(Boss, iTween.Hash("x", Boss.transform.position.x + 10.0f, "time", 1.0f, "easetype", iTween.EaseType.easeOutBounce));
+    }
+    private void moveImage(int currentDialogueIndex)
+    {
+        if (currentDialogueIndex == 0) return;
+        if (currentDialogueIndex % 2 == 0) 
+        iTween.ShakePosition(Me, iTween.Hash("amount",new Vector3( 0.2f,0.1f,0.1f), "time", 1.0f));
+        else
+        {
+            iTween.ShakePosition(Boss, iTween.Hash("amount", new Vector3(0.2f, 0.1f, 0.1f), "time", 1.0f));
+
+        }
     }
 }
