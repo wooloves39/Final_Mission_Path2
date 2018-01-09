0c0ba50916590ef3691250f99d66a7549b1f6c66
wooloves39 <wooloves39@users.noreply.github.com>
2018-01-10 01:27:37 +0900

    stage test/ vr ui-ing

--- Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/MainTitle/TitleScene.cs
+++ Final_Mission/Rucid_witch/Reqid_Witch/Assets/Script/MainTitle/TitleScene.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class TitleScene : MonoBehaviour
 {
-    public GameObject Title;
-    Color object_Color;
-    float opening_time = 0.0f;
-    float opening_color = 0.0f;
-    float Screen_width;
-    float Screen_height;
-    // Use this for initialization
-    private void Awake()
+    public SpriteRenderer Title;
+	Color object_Color;
+	float opening_time = 0.0f;
+	float Alpha_color = 0.0f;
+	float Screen_width;
+	float Screen_height;
+
+	private IEnumerator opening;
+	// Use this for initialization
+	private void Awake()
     {
         // 스크린 해상도 맞추기
         //실제 플레이에선 3번째 인자 true
@@ -22,30 +25,45 @@ public class TitleScene : MonoBehaviour
     }
     void Start()
     {
-        object_Color = Title.transform.GetComponent<MeshRenderer>().material.color;
-
-        Vector3 scale=Title.transform.localScale;
-        Title.gameObject.transform.localScale=new Vector3(scale.x*Screen_width*25/48*0.005f, scale.y*Screen_height * 25/48*0.005f,1);
-        Title.transform.GetComponent<MeshRenderer>().material.color = new Color(object_Color.r, object_Color.g, object_Color.b, opening_color);
-    }
+        object_Color = Title.color;
+		Vector3 scale = Title.transform.localScale;
+		Title.gameObject.transform.localScale = new Vector3(scale.x * Screen_width * 25 / 48 * 0.005f, scale.y * Screen_height * 25 / 48 * 0.005f, 1);
+		
+		Title.color = new Color(object_Color.r, object_Color.g, object_Color.b, 1);
+		opening = OpeningCourutine(Title);
+		StartCoroutine(opening);
+	}
 
     // Update is called once per frame
     void Update()
     {
-        opening_time += Time.deltaTime;
-        if (opening_time < 3.0f)
-        {
-
-            opening_color = (opening_time / 3);
-        }
-        else
-            opening_color = 1;
-        if (Title.gameObject)
-            Title.transform.GetComponent<MeshRenderer>().material.color = new Color(object_Color.r, object_Color.g, object_Color.b, opening_color);
-        if (opening_time > 5.0f)
-        {
-            Destroy(Title.gameObject);
-            //SceneManager.LoadScene("next Scene");
-        }
+		opening_time += Time.deltaTime;
+		if (Input.anyKey&&Alpha_color==1.0f)
+		{
+			Title.gameObject.SetActive(false);
+		}
+		if (Alpha_color == 1.0f)
+		{
+			Debug.Log(Alpha_color);
+			StopCoroutine(opening);
+		}
     }
+	public IEnumerator OpeningCourutine(SpriteRenderer target)
+	{
+		while (true)
+		{
+			Alpha_color += 0.05f;
+			if (Title.gameObject)
+				target.color =
+				new Color(object_Color.r, object_Color.g, object_Color.b, Alpha_color);
+			if (Alpha_color >= 1.0f)
+			{
+				Alpha_color = 1.0f;
+				Debug.Log("dd");
+				yield break;
+			}
+			yield return new WaitForSeconds(0.2f);
+
+		}
+	}
 }
