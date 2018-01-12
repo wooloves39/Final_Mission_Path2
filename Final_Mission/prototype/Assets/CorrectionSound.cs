eaa724fad6f52e4154ce48749fe7e8f4e308668d
Kang Hyun Woong <cdcd8894@naver.com>
2018-01-12 14:56:52 +0900

    Revert "Title UI 최종본"
    This reverts commit 2f0273af487d5d09dfdd81efbe580be2b1fdb711.

--- Final_Mission/prototype/Assets/CorrectionSound.cs
+++ Final_Mission/prototype/Assets/CorrectionSound.cs
@@ -3,17 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CorrectionSound : MonoBehaviour {
-	public float Volume = 0.66f;
+
 	AudioSource audioSource;
 	// Use this for initialization
 	void Start () {
 		audioSource = GetComponent<AudioSource> ();
-		audioSource.volume = Singletone.Instance.Sound;
-	}
-	void ChangeVolume(float v)
-	{
-		Volume = v;
-		Singletone.Instance.Sound = Volume;
 		audioSource.volume = Singletone.Instance.Sound;
 	}
 	void OnLevelWasLoaded()
@@ -26,4 +20,3 @@ public class CorrectionSound : MonoBehaviour {
 		DontDestroyOnLoad (this);
 	}
 }
-
